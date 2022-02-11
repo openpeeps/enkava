7ce37fd9330b1c21fb5daee7d62f08a5156be9c2
George Lemon <georgelemon@protonmail.com>
2022-02-11 22:30:54 +0200

    Add Parrot AST Nodes
    Signed-off-by: George Lemon <georgelemon@protonmail.com>

---
+++ src/parrot/ast.nim
@@ -0,0 +1,89 @@
+from ./lexer import TokenKind
+
+type
+    ParrotTypeValue* = enum
+        TypeAscii
+        TypeAlphabetical
+        TypeBase32
+        TypeBase58
+        TypeBase64
+        TypeBIC
+        TypeBTC
+        TypeCurrency
+        TypeDate
+        TypeDigit
+        TypeEAN
+        TypeEhterum
+        TypeEmail
+        TypeHash
+        TypeHex
+        TypeHexcolor
+        TypeHSL
+        TypeIP
+        TypeIBAN
+        TypeISBN
+        TypeISIN
+        TypeLowercase
+        TypeMacAddress
+        TypeMagnetURI
+        TypeMD5
+        TypeNumerical
+        TypeURL
+        TypeUppercase
+        TypeArray
+        TypeBool
+        TypeFloat
+        TypeInt
+        TypeObject
+        TypeNull
+        TypeString
+        TypeInvalid
+
+    Node* = object
+        identifier*: string
+        typeValue*: ParrotTypeValue
+
+    Rule* = object
+        node*: Node
+        required*: bool
+        defaultValue*: string
+        meta*: tuple[col, line, wsno: int]
+
+proc typeValueByKind*(kind: TokenKind): ParrotTypeValue =
+    return case kind:
+        of TK_ASCII:           TypeAscii
+        of TK_ALPHABETICAL:    TypeAlphabetical
+        of TK_BASE32:          TypeBase32
+        of TK_BASE58:          TypeBase58
+        of TK_BASE64:          TypeBase64
+        of TK_BIC:             TypeBIC
+        of TK_BTC:             TypeBTC
+        of TK_CURRENCY:        TypeCurrency
+        of TK_DATE:            TypeDate
+        of TK_DIGIT:           TypeDigit
+        of TK_EAN:             TypeEAN
+        of TK_ETHERUM:         TypeEhterum
+        of TK_EMAIL:           TypeEmail
+        of TK_HASH:            TypeHash
+        of TK_HEX:             TypeHex
+        of TK_HEXCOLOR:        TypeHexcolor
+        of TK_HSL:             TypeHSL
+        of TK_IP:              TypeIP
+        of TK_IBAN:            TypeIBAN
+        of TK_ISBN:            TypeISBN
+        of TK_ISIN:            TypeISIN
+        of TK_LOWERCASE:       TypeLowercase
+        of TK_MACADDRESS:      TypeMacAddress
+        of TK_MAGNETURI:       TypeMagnetURI
+        of TK_MD5:             TypeMD5
+        of TK_NUMERICAL:       TypeNumerical
+        of TK_URL:             TypeURL
+        of TK_UPPERCASE:       TypeUppercase
+        of TK_TYPE_ARRAY:      TypeArray
+        of TK_TYPE_BOOL:       TypeBool
+        of TK_TYPE_FLOAT:      TypeFloat
+        of TK_TYPE_INT:        TypeInt
+        of TK_TYPE_OBJECT:     TypeObject
+        of TK_TYPE_NULL:       TypeNull
+        of TK_TYPE_STRING:     TypeString
+        else: TypeInvalid
